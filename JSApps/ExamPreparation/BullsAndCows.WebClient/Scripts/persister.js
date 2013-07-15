259a2615ebf8626f99cd6fd8027794b80bef9101
Stann <stoychev.st2@gmail.com>
2013-07-15 17:53:49 +0300

    Final JS Apps HW

--- JSApps/ExamPreparation/BullsAndCows.WebClient/Scripts/persister.js
+++ JSApps/ExamPreparation/BullsAndCows.WebClient/Scripts/persister.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="http-requester.js" />
+/// <reference path="class.js" />
+
+//encrypting is done with: authCode: CryptoJS.SHA1(user.username + user.password).toString()
+
+var persisters = (function () {
+
+}());
+
+var user = {
+    username: "Joro",
+    password: "joro"
+}
